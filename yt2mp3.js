1adaf155f357d11ca5014a1a5c091e39b45e6239
tqp <timothyqpowers@gmail.com>
2022-11-03 16:53:51 -0400

    Update yt2mp3.js
    Updated Regex to remove special characters. This will need to be refined as more special characters are found.

--- yt2mp3.js
+++ yt2mp3.js
@@ -4,10 +4,7 @@ const fs = require('fs');
 const path = 'C:\\_TQP\\music_to_file';
 
 const urlList = [
-    'https://www.youtube.com/watch?v=bgv_YB4Kvbc',
-    'https://www.youtube.com/watch?v=YsMB0i5YTOc',
-    'https://www.youtube.com/watch?v=2xcx26_z4As',
-    'https://www.youtube.com/watch?v=BBHfk4-KBtM'
+    'https://www.youtube.com/watch?v=etr7UtnUflM'
 ];
 
 const getVideoTitle = url =>
@@ -23,11 +20,13 @@ const downloadAll = async () => {
     let url;
     for (url of urlList) {
         const title = await getVideoTitle(url);
-        console.log(`Downloading ${title}`);
+        // const cleanTitle = title.replace(/[^a-zA-Z ]/g, "");
+        const cleanTitle = title.replace(/[$@%]/g, '');
+        console.log(`Downloading ${cleanTitle} (${url})`);
         ytdl(url, {
             format: 'mp3',
             filter: 'audioonly',
-        }).pipe(fs.createWriteStream(path + '\\' + title + '.mp3'));
+        }).pipe(fs.createWriteStream(path + '\\' + cleanTitle + '.mp3'));
     }
     console.log('Finished');
 };
